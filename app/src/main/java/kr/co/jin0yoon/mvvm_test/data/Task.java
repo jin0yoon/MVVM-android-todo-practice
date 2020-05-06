69ec79c6e25bc017d76af211a158fe2cd9b94c58
jin0yoon <wwwz4805@kakao.com>
2020-05-06 17:27:13 +0900

    Create Task.java
    task is immutable model class for a Task

---
+++ app/src/main/java/kr/co/jin0yoon/mvvm_test/data/Task.java
@@ -0,0 +1,180 @@
+/*
+ * Copyright 2016, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package kr.co.jin0yoon.mvvm_test.data;
+
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+import com.google.common.base.Objects;
+import com.google.common.base.Strings;
+
+import java.util.UUID;
+
+/**
+ * Immutable model class for a Task.
+ * 변경할 수 없는 model class
+ */
+@Entity(tableName = "tasks")
+public final class Task {
+
+    //id를 PK로 지정
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "entryid")
+    private final String mId;
+
+    @Nullable
+    @ColumnInfo(name = "title")
+    private final String mTitle;
+
+    @Nullable
+    @ColumnInfo(name = "description")
+    private final String mDescription;
+
+    @ColumnInfo(name = "completed")
+    private final boolean mCompleted;
+
+    /**
+     * Use this constructor to create a new active Task.
+     *
+     * @param title       title of the task
+     * @param description description of the task
+     */
+
+    //새로운 active Task를 만들기 위한 생성자
+    //새로은 Task이므로 random 값의 UUID를 id 값으로 지정
+    @Ignore
+    public Task(@Nullable String title, @Nullable String description) {
+        this(title, description, UUID.randomUUID().toString(), false);
+    }
+
+    /**
+     * Use this constructor to create an active Task if the Task already has an id (copy of another
+     * Task).
+     *
+     * @param title       title of the task
+     * @param description description of the task
+     * @param id          id of the task
+     */
+
+    //Task가 이미 id를 가지고 있는 경우 (다른 Task의 복사)를 위한 생성자
+    //이미 존재하는 id를 파라미터로 받아와서 id 값으로 지정
+    @Ignore
+    public Task(@Nullable String title, @Nullable String description, @NonNull String id) {
+        this(title, description, id, false);
+    }
+
+    /**
+     * Use this constructor to create a new completed Task.
+     *
+     * @param title       title of the task
+     * @param description description of the task
+     * @param completed   true if the task is completed, false if it's active
+     */
+
+    //새로운 completed Task를 만들기 위한 생성자
+    //completed 여부를 파라미터로 받아옴
+    //새로은 Task이므로 random 값의 UUID를 id 값으로 지정
+    @Ignore
+    public Task(@Nullable String title, @Nullable String description, boolean completed) {
+        this(title, description, UUID.randomUUID().toString(), completed);
+    }
+
+    /**
+     * Use this constructor to specify a completed Task if the Task already has an id (copy of
+     * another Task).
+     *
+     * @param title       title of the task
+     * @param description description of the task
+     * @param id          id of the task
+     * @param completed   true if the task is completed, false if it's active
+     */
+
+    //이미 id가 존재하는 경우(다른 Task의 복사) completed Task를 명시하기 위한 생성자
+    //이미 존재하는 id를 파라미터로 받아와서 id 값으로 지정
+    //completed 여부를 파라미터로 받아옴
+    public Task(@Nullable String title, @Nullable String description,
+                @NonNull String id, boolean completed) {
+        mId = id;
+        mTitle = title;
+        mDescription = description;
+        mCompleted = completed;
+    }
+
+    //get methods
+    @NonNull
+    public String getId() {
+        return mId;
+    }
+
+    @Nullable
+    public String getTitle() {
+        return mTitle;
+    }
+
+    @Nullable
+    public String getTitleForList() {
+        if (!Strings.isNullOrEmpty(mTitle)) {
+            return mTitle;
+        } else {
+            return mDescription;
+        }
+    }
+
+    @Nullable
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public boolean isCompleted() {
+        return mCompleted;
+    }
+
+    public boolean isActive() {
+        return !mCompleted;
+    }
+
+    public boolean isEmpty() {
+        return Strings.isNullOrEmpty(mTitle) &&
+               Strings.isNullOrEmpty(mDescription);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equal(mId, task.mId) &&
+               Objects.equal(mTitle, task.mTitle) &&
+               Objects.equal(mDescription, task.mDescription);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(mId, mTitle, mDescription);
+    }
+
+    @Override
+    public String toString() {
+        return "Task with title " + mTitle;
+    }
+}
